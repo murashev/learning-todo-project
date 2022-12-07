b7434bb843cf66de7b556739f10a72c2d4cd20d3
murashev <mikas.murashev@gmail.com>
2022-12-07 21:11:41 +0400

    learning project is done

---
+++ 00 todo start project/js/todo/model.js
@@ -0,0 +1,74 @@
+export default class Model {
+
+   constructor() {
+      this.tasks = [];
+      this.loadFromLocalStorage();
+   }
+
+   loadFromLocalStorage() {
+      const data = localStorage.getItem('tasks');
+      if (data) {
+         this.tasks = JSON.parse(data);
+      }
+   }
+
+   saveToLocalStorage() {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+   }
+
+   addTask(text) {
+
+      let id = 1;
+
+      if (this.tasks.length > 0) {
+         id = this.tasks[this.tasks.length - 1]['id'] + 1;
+      }
+
+      const newTask = {
+         id: id,
+         status: 'active',
+         text: text,
+      };
+
+      this.tasks.push(newTask);
+      this.saveToLocalStorage();
+
+      return newTask;
+   }
+
+   findTask(id) {
+      const task = this.tasks.find(function (task) {
+         if (task.id === parseInt(id)) {
+            return true;
+         }
+      })
+      return task;
+   }
+
+   changeStatus(task) {
+
+      if (task.status === 'active') {
+         task.status = 'done';
+      } else {
+         task.status = 'active';
+      }
+
+      this.saveToLocalStorage();
+   }
+
+   removeTask(task) {
+      const index = this.tasks.indexOf(task);
+      this.tasks.splice(index, 1);
+      this.saveToLocalStorage();
+   }
+}
+
+// tasks = ['Заверстать стартовый шаблон', 'Написать скрипт', 'Записать урок', '465465464'];
+// tasks = [
+//     {
+//         status: 'active',
+// 		text: 'Заверстать стартовый шаблон',
+// 	},
+// 	{},
+// 	{},
+// ];
